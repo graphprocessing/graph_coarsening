529ca13d287c10b2ae76f84e364a7a13be9a777f
Nesterov Alexander <nesoldr@mail.ru>
2018-10-27 01:37:22 +0300

    Added CI pipeline

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
+python scripts/lint.py
+
 if [ ! -d "build" ]; then
     mkdir build
 fi
 
 cd build
-cmake ..
+cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
+cppcheck -j4 --project=compile_commands.json &> log_cppcheck
+python ../scripts/static_analysis.py log_cppcheck
 make -j4
 ./modules/application/Graph_Partition
 cd ..
-
--- modules/application/main.cpp
+++ modules/application/main.cpp
@@ -1,3 +1,4 @@
+// Copyright [year] <Copyright Owner>
 #include <iostream>
 
 int main(int argc, char** argv) {
