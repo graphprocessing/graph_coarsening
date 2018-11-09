972260d191614bda11ab18ad0eabf06102f07635
Nesterov Alexander <nesoldr@mail.ru>
2018-11-09 14:45:19 +0000

    Refactor prototype

--- modules/application/main.cpp
+++ modules/application/main.cpp
@@ -1,5 +1,7 @@
 // Copyright [year] <Copyright Owner>
 #include <iostream>
+#include <vector>
+#include "../data_structures/include/graph.h"
 
 int main(int argc, char** argv) {
     std::cout << "This start application!" << '\n';
