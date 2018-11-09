5e7bb49ee90e77985a2e5c53948117fe0126fa03
Nesterov Alexander <nesoldr@mail.ru>
2018-11-09 22:22:47 +0000

    Added google tests framework

--- build.sh
+++ build.sh
@@ -9,5 +9,6 @@ cmake -D CMAKE_BUILD_TYPE=Release -DCMAKE_EXPORT_COMPILE_COMMANDS=ON ..
 cppcheck -j4 --project=compile_commands.json &> log_cppcheck
 python ../scripts/static_analysis.py log_cppcheck
 make -j4
+./tests/Test_Target
 ./modules/application/Graph_Partition
 cd ..
--- scripts/lint.py
+++ scripts/lint.py
@@ -25,7 +25,7 @@ exit_flag = True
 for dirs, node, files in os.walk(project_directory):
     for file in files:
         if re.search(pattern_filename, file) != None:
-            if re.search(r'/build/', dirs) == None:
+            if re.search(r'/build/|/3rdparty/', dirs) == None:
                 os.chdir(dirs)
                 print(dirs + " -> " + file)
                 command = sys.executable + ' ' + cpplint_path + ' ' + file
