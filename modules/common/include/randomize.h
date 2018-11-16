d501c63fcfaa62c13c38a0a70554f84558e074bc
Arseniy Obolenskiy <gooddoog@student.su>
2018-11-16 20:07:14 +0300

    Fixed some cpplint errors

--- modules/common/include/randomize.h
+++ modules/common/include/randomize.h
@@ -1,8 +1,8 @@
 // Copyright [year] <Copyright Owner>
-#ifndef MODULES_COMMON_INCLUDE_RANDOMIZE_H
-#define MODULES_COMMON_INCLUDE_RANDOMIZE_H
+#ifndef MODULES_COMMON_INCLUDE_RANDOMIZE_H_
+#define MODULES_COMMON_INCLUDE_RANDOMIZE_H_
 #include <vector>
 
 bool randomize_permutation(std::vector <int>* permutation, unsigned dist);
 
-#endif  // MODULES_COMMON_INCLUDE_RANDOMIZE_H
+#endif  // MODULES_COMMON_INCLUDE_RANDOMIZE_H_
