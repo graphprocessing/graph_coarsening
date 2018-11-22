51ada2a3c427c82c15c4ac2e6a810677062db0eb
Arseniy Obolenskiy <gooddoog@student.su>
2018-11-22 23:00:31 +0300

    Added test for random_matching on zadeh_test

--- tests/include/random_matching_parametric_test.h
+++ tests/include/random_matching_parametric_test.h
@@ -18,7 +18,7 @@ class random_matching_parametric_test : public testing::TestWithParam<int> {
         value = GetParam();
     }
 
-    void do_test_1() {
+    void correct_random_matching_on_csr_washington_test() {
         CSR csr = washington_test(value);
         std::vector <char> used(csr.n, 0);
         auto matching = random_matching(csr);
@@ -27,12 +27,12 @@ class random_matching_parametric_test : public testing::TestWithParam<int> {
         for (int i = 0; i < csr.n; ++i) {
             if (!used[i]) {
                 for (int j = csr.offset[i]; j < csr.offset[i+1]; ++j)
-                    ASSERT_EQ(true, used[csr.edges[j].first]);
+                    ASSERT_NE(0, used[csr.edges[j].first]);
             }
         }
     }
 
-    void do_test_2() {
+    void correct_random_matching_on_al_washington_test() {
         AL al = washington_test(value);
         std::vector <char> used(al.n, 0);
         auto matching = random_matching(al);
@@ -41,7 +41,35 @@ class random_matching_parametric_test : public testing::TestWithParam<int> {
         for (int i = 0; i < al.n; ++i) {
             if (!used[i]) {
                 for (unsigned j = 0; j < al.edges[i].size(); ++j)
-                    ASSERT_EQ(true, used[al.edges[i][j].first]);
+                    ASSERT_NE(0, used[al.edges[i][j].first]);
+            }
+        }
+    }
+
+    void correct_random_matching_on_csr_zadeh_test() {
+        CSR csr = zadeh_test(value);
+        std::vector <char> used(csr.n, 0);
+        auto matching = random_matching(csr);
+        for (int i = 0; i < matching.n; ++i)
+            used[matching.edge_b[i]] = used[matching.edge_e[i]] = 2;
+        for (int i = 0; i < csr.n; ++i) {
+            if (!used[i]) {
+                for (int j = csr.offset[i]; j < csr.offset[i+1]; ++j)
+                    ASSERT_NE(0, used[csr.edges[j].first]);
+            }
+        }
+    }
+
+    void correct_random_matching_on_al_zadeh_test() {
+        AL al = zadeh_test(value);
+        std::vector <char> used(al.n, 0);
+        auto matching = random_matching(al);
+        for (int i = 0; i < matching.n; ++i)
+            used[matching.edge_b[i]] = used[matching.edge_e[i]] = 3;
+        for (int i = 0; i < al.n; ++i) {
+            if (!used[i]) {
+                for (unsigned j = 0; j < al.edges[i].size(); ++j)
+                    ASSERT_NE(0, used[al.edges[i][j].first]);
             }
         }
     }
@@ -55,22 +83,34 @@ std::vector <int> generate_samples(int n) {
     std::mt19937 generator(rd());
     std::vector <int> result;
     for (int i = 0; i < n; ++i)
-        result.push_back(std::uniform_int_distribution<int>(1e5, 1e6)
+        result.push_back(std::uniform_int_distribution<int>(1, 1e3)
                                 (generator));
     return result;
 }
 
 std::vector <int> samples = generate_samples(10);
 
-TEST_P(random_matching_parametric_test, correct_random_matching_on_csr) {
-    do_test_1();
+TEST_P(random_matching_parametric_test,
+                    correct_random_matching_on_csr_washington_test) {
+    correct_random_matching_on_csr_washington_test();
 }
 
-TEST_P(random_matching_parametric_test, correct_random_matching_on_al) {
-    do_test_2();
+TEST_P(random_matching_parametric_test,
+                    correct_random_matching_on_al_washington_test) {
+    correct_random_matching_on_al_washington_test();
+}
+
+TEST_P(random_matching_parametric_test,
+                    correct_random_matching_on_csr_zadeh_test) {
+    correct_random_matching_on_csr_zadeh_test();
+}
+
+TEST_P(random_matching_parametric_test,
+                    correct_random_matching_on_al_zadeh_test) {
+    correct_random_matching_on_al_zadeh_test();
 }
 
 INSTANTIATE_TEST_CASE_P(random_matching_test, random_matching_parametric_test,
-                        testing::ValuesIn(samples));
+                    testing::ValuesIn(samples));
 
 #endif  // TESTS_INCLUDE_RANDOM_MATCHING_PARAMETRIC_TEST_H_
