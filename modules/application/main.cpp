92c542ab8ed96cfa6164267c30a86f2e3c542231
Arseniy Obolenskiy <gooddoog@student.su>
2018-12-12 22:58:40 +0300

    Removed whitespace

--- modules/application/main.cpp
+++ modules/application/main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv) {
                 << Timer::calculate(washington_test, 2, false)
                 << " s" << std::endl << std::endl;
     std::cout << "Execution time for washington_test (n = 1e6): "
-                << Timer::calculate(washington_test, 
+                << Timer::calculate(washington_test,
                     static_cast<int>(1e6), false)
                 << " s" << std::endl << std::endl;
     return 0;
