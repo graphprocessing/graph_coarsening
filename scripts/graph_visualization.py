567caf6d55b3453561d35596a7141cb75e8cb86c
Arseniy Obolenskiy <gooddoog@student.su>
2018-12-30 21:47:11 +0300

    Improved graph_visualization.py script

--- scripts/graph_visualization.py
+++ scripts/graph_visualization.py
@@ -2,11 +2,11 @@ import networkx as nx
 import matplotlib.pyplot as plt
 import matplotlib.pylab as plb
 import sys
+from datetime import datetime
 
 file = sys.argv[1]
-png_scale = 2
-fig = plt.gcf()
-fig.set_size_inches(fig.get_size_inches()[0] * png_scale, fig.get_size_inches()[1] * png_scale)
+png_scale = 3
+system_time = datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
 if __name__ == "__main__":
     graph = nx.DiGraph()
@@ -15,11 +15,16 @@ if __name__ == "__main__":
         data = line.split()
         graph.add_edge(data[0], data[1], weight=data[2])
     f.close()
-    plt.figure()
+    fig = plt.figure(1)
+    fig.set_size_inches(fig.get_size_inches()[0] * png_scale, fig.get_size_inches()[1] * png_scale)
+    plt.title(system_time + " graph")
     plt.axis("off")
-    layout = nx.spring_layout(graph)
-    nx.draw_networkx_nodes(graph, layout)
-    nx.draw_networkx_edges(graph, layout)
-    nx.draw_networkx_labels(graph, layout)
-    plt.savefig("out.png", format="png")
+    pos = nx.spring_layout(graph)
+    nx.draw_networkx_nodes(graph, pos)
+    nx.draw_networkx_edges(graph, pos)
+    weights = nx.get_edge_attributes(graph, 'weight')
+    nx.draw_networkx_labels(graph, pos)
+    if input("Display graph weights? (y/n)\n> ")[0].lower() == 'y':
+        nx.draw_networkx_edge_labels(graph, pos, edge_labels=weights)
+    plt.savefig(system_time + "_graph.png", format="png")
     plb.close()
