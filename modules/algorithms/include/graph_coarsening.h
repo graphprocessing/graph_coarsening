312a2c53b275d5eb14635f20e874d0fa2d64f5b9
Shramek42 <43232868+Shramek42@users.noreply.github.com>
2019-02-15 11:30:35 +0300

    Graph_coarsening_2.0 (#24)

--- modules/algorithms/include/graph_coarsening.h
+++ modules/algorithms/include/graph_coarsening.h
@@ -6,17 +6,14 @@
 template <typename WeightType>
 CSR<WeightType> graph_coarsening(const CSR<WeightType>& graph,
                                     const Matching& match) {
-    CSR<WeightType> g;
+    CSR <WeightType> g;
     std::map <int, int> Map;
-    std::map <int, std::pair<int, int>> Map1;
     int sz = static_cast<int>(graph.n);
     std::vector <int> new_vertex(sz, 0);
     for (int i = 0; i < match.n; i++) {
         Map[match.edge_b[i]] = i;
         Map[match.edge_e[i]] = i;
-        Map1[i] = std::make_pair(match.edge_b[i], match.edge_e[i]);
-        int weight = 0;
-        graph.offset[match.edge_b[i]];
+        WeightType weight = 0;
         for (int j = graph.offset[match.edge_b[i]];
             j < graph.offset[match.edge_b[i] + 1]; j++) {
             if (graph.edges[j] == match.edge_e[i]) {
@@ -27,51 +24,121 @@ CSR<WeightType> graph_coarsening(const CSR<WeightType>& graph,
         new_vertex[i] = weight;
     }
     int t = match.n - 1;
+    std::map <int, int> Map1;
     for (int i = 0; i < graph.n; i++) {
         if (!Map.count(i)) {
             Map[i] = ++t;
-            Map1[t] = std::make_pair(i, -1);
+            Map1[t] = i;
         }
     }
-    std::vector <bool> used(t);
     g.offset.push_back(0);
-    int vert_sz = 0;
+    int cur_offset_num = 0;
     for (int i = 0; i < match.n; i++) {
-        int weight = new_vertex[i];
-        for (int j = graph.offset[Map1[i].first];
-            j < graph.offset[Map1[i].first + 1]; j++) {
-            int to = Map[graph.edges[j]];
-            if (i != to) {
+        WeightType weight = new_vertex[i];
+        std::set <int> used;
+        int cur_vert = Map[match.edge_b[i]];
+        used.insert(cur_vert);
+        int p1_b = graph.offset[match.edge_b[i]];
+        int p1_e = graph.offset[match.edge_b[i] + 1];
+        int p2_b = graph.offset[match.edge_e[i]];
+        int p2_e = graph.offset[match.edge_e[i] + 1];
+        while (p1_b < p1_e) {
+            int to = Map[graph.edges[p1_b]];
+            if (used.find(to) == used.end()) {
+                used.insert(to);
                 g.edges.push_back(to);
-                g.weights.push_back(graph.weights[j] +
-                                weight + new_vertex[to]);
-                vert_sz++;
-            } else {
-                for (int k = graph.offset[Map1[i].second];
-                        k < graph.offset[Map1[i].second + 1]; k++) {
-                    int to1 = Map[graph.edges[k]];
-                    if (i != to1) {
-                        g.edges.push_back(to1);
-                        g.weights.push_back(graph.weights[k] +
-                                        weight + new_vertex[to1]);
-                        vert_sz++;
+                cur_offset_num++;
+                WeightType weight1 = 0;
+                if (to < match.n) {
+                    for (int j = p1_b, k = 0;
+                                    (j < p1_e) && (k < 2); j++) {
+                        if (match.edge_b[to] == graph.edges[j]) {
+                            weight1 += graph.weights[j];
+                            k++;
+                        }
+                        if (match.edge_e[to] == graph.edges[j]) {
+                            weight1 += graph.weights[j];
+                            k++;
+                        }
                     }
+                    for (int j = p2_b, k = 0;
+                                    (j < p2_e) && (k < 2); j++) {
+                        if (match.edge_b[to] == graph.edges[j]) {
+                            weight1 += graph.weights[j];
+                            k++;
+                        }
+                        if (match.edge_e[to] == graph.edges[j]) {
+                            weight1 += graph.weights[j];
+                            k++;
+                        }
+                    }
+                } else {
+                    weight1 = graph.weights[p1_b];
                 }
+                g.weights.push_back(weight + weight1 +
+                                            new_vertex[to]);
             }
+            p1_b++;
         }
-        g.offset.push_back(vert_sz);
+        while (p2_b < p2_e) {
+            int to = Map[graph.edges[p2_b]];
+            if (used.find(to) == used.end()) {
+                used.insert(to);
+                g.edges.push_back(to);
+                cur_offset_num++;
+                WeightType weight1 = 0;
+                if (to < match.n) {
+                    for (int j = p2_b, k = 0;
+                                    (j < p2_e) && (k < 2); j++) {
+                        if (match.edge_b[to] == graph.edges[j]) {
+                            weight1 += graph.weights[j];
+                            k++;
+                        }
+                        if (match.edge_e[to] == graph.edges[j]) {
+                            weight1 += graph.weights[j];
+                            k++;
+                        }
+                    }
+                } else {
+                    weight1 = graph.weights[p1_b];
+                }
+                g.weights.push_back(weight + weight1 +
+                                            new_vertex[to]);
+            }
+            p2_b++;
+        }
+        g.offset.push_back(cur_offset_num);
     }
     for (int i = match.n; i <= t; i++) {
-        int weight = new_vertex[i];
-         for (int j = graph.offset[Map1[i].first];
-            j < graph.offset[Map1[i].first + 1]; j++) {
-                int to = Map[graph.edges[j]];
+        int p_b = graph.offset[Map1[i]];
+        int p_e = graph.offset[Map1[i] + 1];
+        std::set <int> used;
+        for (int j = p_b; j < p_e; j++) {
+            int to = Map[graph.edges[j]];
+            if (used.find(to) == used.end()) {
+                used.insert(to);
                 g.edges.push_back(to);
-                g.weights.push_back(graph.weights[j] +
-                                weight + new_vertex[to]);
-                vert_sz++;
+                cur_offset_num++;
+                WeightType weight1 = 0;
+                if (to < match.n) {
+                    for (int j1 = j, k = 0;
+                                    (j1 < p_e) && (k < 2); j1++) {
+                        if (match.edge_b[to] == graph.edges[j1]) {
+                            weight1 += graph.weights[j1];
+                            k++;
+                        }
+                        if (match.edge_e[to] == graph.edges[j1]) {
+                            weight1 += graph.weights[j1];
+                            k++;
+                        }
+                    }
+                } else {
+                    weight1 = graph.weights[j];
+                }
+                g.weights.push_back(weight1 + new_vertex[to]);
+            }
         }
-        g.offset.push_back(vert_sz);
+        g.offset.push_back(cur_offset_num);
     }
     g.n = t + 1;
     return g;
