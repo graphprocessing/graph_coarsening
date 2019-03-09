ef420b807550862f54fa608f957e8ad8dd2f3862
Alexandr Lebedev <43033107+XEverentX@users.noreply.github.com>
2019-03-09 22:51:49 +0300

    Added benchmark for Edmonds (#66)
    * Added edmonds
    
    * Added edmonds
    
    * Added personaly tests for Edmonds, Added edmods.h path to precomp.h
    
    * Delete edmonds.h
    
    * Added benchmark tests for edmonds

---
+++ benchmark/include/edmonds_test.h
@@ -0,0 +1,16 @@
+// Copyright [year] <Copyright Owner>
+#ifndef BENCHMARK_INCLUDE_EDMONDS_TEST_H_
+#define BENCHMARK_INCLUDE_EDMONDS_TEST_H_
+
+#include <benchmark/benchmark.h>
+#include "../../modules/pch/include/precomp.h"
+
+void edmonds_on_washington_test_csr(benchmark::State& state);
+void edmonds_on_zadeh_test_csr(benchmark::State& state);
+void edmonds_on_cube_test_csr(benchmark::State& state);
+
+BENCHMARK(edmonds_on_washington_test_csr)->Range(2, 1e4)->Complexity();
+BENCHMARK(edmonds_on_zadeh_test_csr)->Range(2, 1e3)->Complexity();
+BENCHMARK(edmonds_on_cube_test_csr)->Range(2, 50)->Complexity();
+
+#endif  // BENCHMARK_INCLUDE_EDMONDS_TEST_H_
